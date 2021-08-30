9744b594cccf9d26d75a8d87ebffbf1ea6cde601
iamyajat <myajat21@gmail.com>
2021-08-30 23:03:46 +0530

    fix: azure function deployment

--- main.py
+++ main.py
@@ -46,5 +46,5 @@ def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return AsgiMiddleware(app).handle(req, context)
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=5000)
